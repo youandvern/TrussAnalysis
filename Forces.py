24186dabd054b5c7fb877e3a0bd52eef106a5df9
youandvern <ayounguw@gmail.com>
2022-03-31 08:03:25 -0700

    Refactor force matrix class

--- Forces.py
+++ Forces.py
@@ -1,19 +1,20 @@
-from PrattTrussGeometry.Geometry import Geometry
-
-
 class Forces(object):
     """
     Class to generate force matrix for Pratt Truss geometry
     """
-    def __init__(self, geometry):
-        self.geometry = geometry
-        self.forces = [0] * geometry.getNNodes()*2
+    def __init__(self, nNodes):
+        self.forces = [0] * nNodes*2
+
+    # resets current force matrix to all 0s
+    def resetForces(self):
+        self.forces = [0] * len(self.forces)
 
     # positive direction is right and up
     def setForceAtNode(self, nodeIndex, forceX=0, forceY=0):
         self.forces[2 * nodeIndex] = forceX
         self.forces[2 * nodeIndex + 1] = forceY
 
+    # positive direction is right and up
     def setForceAtNodes(self, nodeIndexList, forceX=0, forceY=0):
         for i in nodeIndexList:
             self.setForceAtNode(i, forceX, forceY)
