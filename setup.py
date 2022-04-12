138354e298825caa1f2e407e566a17894b34bc13
Andrew Young <67914889+youandvern@users.noreply.github.com>
2022-04-12 16:28:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TrussAnalysis',
-    version='0.1',
+    version='0.1.1',
     packages=['PrattTrussGeometry'],
     url='https://github.com/youandvern/TrussAnalysis',
     license='MIT',
