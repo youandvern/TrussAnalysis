669d7da5090f3e760edf937af5f8f2f0c7b3a805
youandvern <ayounguw@gmail.com>
2023-06-27 11:34:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TrussAnalysis',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(),
     url='https://github.com/youandvern/TrussAnalysis',
     license='MIT',
